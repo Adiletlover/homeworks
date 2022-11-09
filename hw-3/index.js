f58c916c62bf5c309062bbf98345162548db06df
A <116088102+Adiletlover@users.noreply.github.com>
2022-11-09 15:42:46 +0600

    Add files via upload
    homework - 3

---
+++ hw-3/index.js
@@ -0,0 +1,32 @@
+//======================
+//     FIRST
+//======================
+
+function twoTimes (a,b) {
+    console.log(a ** b)
+}
+twoTimes (3,2)
+
+
+//======================
+//     SECOND
+//======================
+
+// array =[]
+// function bigCoin (array) {
+//     if (array % 2){
+//         return
+//         }
+//     }
+// console.log(bigCoin(2,4,5,6,7,8))
+
+
+//======================
+//     Third
+//======================
+
+
+function yesSir (a,b){
+    console.log((a+b) / 2)
+}
+yesSir(3,3)
