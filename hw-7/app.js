6dff5dd51d4a98915203a2fb6b11ab13d4a360e5
A <116088102+Adiletlover@users.noreply.github.com>
2022-11-18 17:58:16 +0600

    Add files via upload
    homework-7

---
+++ hw-7/app.js
@@ -0,0 +1,58 @@
+//------------------------------------------------------------------------------------
+// Домашнее задание: 
+// Реализуйте класс Worker (Работник), который будет иметь следующие свойства: name (имя), surname (фамилия), rate (ставка за день работы), days (количество отработанных дней). Также класс должен иметь метод getSalary(), который будет выводить зарплату работника. Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
+// С помощью нашего класса создайте двух рабочих и найдите сумму их зарплат.
+//------------------------------------------------------------------------------------
+
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+    getSalary(rate, days) {
+        let salary = this.rate * this.days;
+        return salary;
+    }
+}
+let Adilet = new Worker('Adilet', 'Jusupov', 225, 20);
+console.log(Adilet.getSalary());
+let Joe = new Worker('Joe', 'Martin', 500, 25);
+console.log(Joe.getSalary());
+
+//------------------------------------------------------------------------------------
+// Доп:
+// Реализуйте класс MyString, который будет иметь следующие методы: метод reverse(), который параметром принимает строку, а возвращает ее в перевернутом виде, метод ucFirst(), который параметром принимает строку, а возвращает эту же строку, сделав ее первую букву заглавной и метод ucWords, который принимает строку и делает заглавной первую букву каждого слова этой строки.
+//------------------------------------------------------------------------------------
+
+class MyString {
+    reverse(str) {
+        let reversStr = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+            reversStr += str[i];
+        }
+        return reversStr;
+    }
+    ucFirst(str) {
+        let newStr = str[0].toUpperCase() + str.slice(1);
+        return newStr;
+    }
+    ucWords(str) {
+        let arr = str.split(' ');
+        let strArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            let a = arr[i];
+            let newStr = a[0].toUpperCase() + a.slice(1);
+            strArr.push(newStr);
+        }
+        return strArr.join(' ');
+    }
+}
+let yourWords = new MyString();
+console.log(yourWords.reverse('proverka odin'));
+console.log(yourWords.ucFirst('proverka dva'));
+console.log(yourWords.ucWords('Proverka tri'));
+
+//------------------------------------------------------------------------------------|
+//------------------------------------------------------------------------------------|
